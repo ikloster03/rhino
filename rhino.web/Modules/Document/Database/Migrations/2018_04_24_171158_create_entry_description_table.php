373fb934630a8d9b4e23ffa027d91df2b1e47aca
Alex <samcro00@yandex.ru>
2018-04-29 14:15:43 +0300

    [*] fix class name migration

--- rhino.web/Modules/Document/Database/Migrations/2018_04_24_171158_create_entry_description_table.php
+++ rhino.web/Modules/Document/Database/Migrations/2018_04_24_171158_create_entry_description_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDocumentTable extends Migration
+class CreateEntryDescriptionTable extends Migration
 {
     /**
      * Run the migrations.
